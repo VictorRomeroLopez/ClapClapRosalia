bc45cfd5a3b5a515096d6de00b34ef21dcdbba84
Andreu Margarit <andreu.margarit@enti.cat>
2020-02-13 11:51:04 +0100

    Countdown

---
+++ Assets/CountDown.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDown : MonoBehaviour
+{
+    [SerializeField]
+    private Text timeText;
+    private float maxTime = 30f;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateCounter();
+    }
+
+    void UpdateCounter()
+    {
+        maxTime -= Time.deltaTime;
+        int secondValue = (int)maxTime % 60;
+        timeText.text = secondValue.ToString();
+    }
+}
