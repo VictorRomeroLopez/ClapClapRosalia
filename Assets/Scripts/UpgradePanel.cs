a7e3bf7402062cdcf43b98778b8760a670d1a6f6
VictorRomeroLopez <victor.romero@enti.cat>
2020-02-20 07:58:41 +0100

    escalable menu

---
+++ Assets/Scripts/UpgradePanel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class UpgradePanel : MonoBehaviour
+{
+    [Header("Tittle Canvas")]
+    [SerializeField] private TextMeshProUGUI _tittleText;
+    [SerializeField] private TextMeshProUGUI _pointsText;
+    [SerializeField] private TextMeshProUGUI _levelText;
+
+    [Space, Header("Cost Canvas")]
+    [SerializeField] private TextMeshProUGUI _costText;
+
+    [Space, Header("Upgrade Canvas")]
+    [SerializeField] private Button _upgradeButton;
+
+    public string TittleText { get => _tittleText.text; set => _tittleText.text = value; }
+    public string PointsText { get => _pointsText.text; set => _pointsText.text = value; }
+    public string LevelText { get => _levelText.text; set => _levelText.text = value; }
+    public string CostText { get => _costText.text; set => _costText.text = value; }
+    public Button UpgradeButton { get => _upgradeButton; set => _upgradeButton = value; }
+
+}
